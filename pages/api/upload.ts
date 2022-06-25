f42f6ea50b7d0e03555ce1ab9b6eb8d2b56473fe
Nguyen Hanh <ndhanh96@gmail.com>
2022-06-26 01:59:59 +0700

    first upload

---
+++ pages/api/upload.ts
@@ -0,0 +1,27 @@
+import multer from 'multer';
+import { NextApiResponse } from 'next';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+var storage = multer.diskStorage({
+  destination: './public/uploads',
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+export default function handler(req:any, res: NextApiResponse) {
+  upload.array('photo', 1)(req, {}, (err) => {
+    // do error handling here
+    console.log(req.files); // do something with the files here
+    res.status(200).send(req.files);
+  });
+  
+  
+}
